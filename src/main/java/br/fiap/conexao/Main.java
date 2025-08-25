e6abc3d85ba633f73133b6983f8d403736cae28d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-25 20:59:02 -0300

    Criando método listar para tabela despesa

--- src/main/java/br/fiap/conexao/Main.java
+++ src/main/java/br/fiap/conexao/Main.java
@@ -2,17 +2,30 @@ package br.fiap.conexao;
 
 import br.fiap.modelo.Categoria;
 import br.fiap.modelo.CategoriaDAO;
+import br.fiap.modelo.Despesa;
+import br.fiap.modelo.DespesaDAO;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         CategoriaDAO dao = new CategoriaDAO();
-//        Categoria categoria = new Categoria(8L, "festa");
-//        dao.inserir(categoria);
+        DespesaDAO deDao = new DespesaDAO();
+        Despesa despesa = new Despesa();
+        //DateTimeFormatter mascara = new DateTimeFormatter();
         List<Categoria> lista = dao.listar();
         for (Categoria categoria : lista) {
             System.out.println(categoria.getId() + " --> " + categoria.getCategoria());
         }
+
+        System.out.println("---------");
+
+        List<Despesa> listaDespesa = deDao.listar();
+        for (Despesa despesa : listaDespesa) {
+            System.out.println(despesa.getId() + " --> " + despesa.getDescricao() + " --> " + despesa.getValor());
+        }
+
     }
 }
--- src/main/java/br/fiap/modelo/DespesaDAO.java
+++ src/main/java/br/fiap/modelo/DespesaDAO.java
@@ -1,9 +1,10 @@
 package br.fiap.modelo;
 
 import br.fiap.conexao.Conexao;
-import sun.security.krb5.internal.crypto.Des;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DespesaDAO {
 
@@ -12,10 +13,9 @@ public class DespesaDAO {
     private String sql;
 
     public void inserir (Despesa despesa){
-        sql = "insert into java_categoria values (?, ?, ?, ?, ?)";
+        sql = "insert into java_categoria values (seqdes.nextval, ?, ?, ?, ?)";
         try (Connection connection = Conexao.conectar()) {
             ps = connection.prepareStatement(sql);
-            ps.setLong(1, despesa.getId());
             ps.setString(2, despesa.getDescricao());
             ps.setDouble(3, despesa.getValor());
             ps.setDate(4, Date.valueOf(despesa.getData()));
@@ -24,6 +24,27 @@ public class DespesaDAO {
         } catch (SQLException e) {
             System.out.println("Erro ao inserir dado no banco " + e);
         }
+    }
+
+    public List<Despesa> listar () {
+        List<Despesa> lista = new ArrayList<>();
+        sql = "select * from java_despesa";
+        try (Connection connection = Conexao.conectar()) {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                Despesa despesa = new Despesa();
+                despesa.setId(rs.getLong("id_despesa"));
+                despesa.setDescricao(rs.getString("descricao"));
+                despesa.setValor(rs.getDouble("valor"));
+                despesa.setCategoria(new Categoria(rs.getLong("id_categoria"), ""));
+                despesa.setData(rs.getDate("date").toLocalDate());
+                lista.add(despesa);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ocorreu um erro: " + e);
+        }
+        return lista;
     }
 
 }
